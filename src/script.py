012187a7c090dc44d7403e70433802f703de46ad
OGreeni <omri.green1@gmail.com>
2024-07-10 21:36:02 -0400

    add .env file

--- src/script.py
+++ src/script.py
@@ -1,14 +1,19 @@
 import praw
 import cleaning
+from dotenv import load_dotenv
+import os
+
+# Load env variables from .env
+load_dotenv()
 
 # List of subreddits to use
 subreddits = ["Investing", "StockMarket", "Stocks", "Trading"]
 
 # Create a reddit client
 reddit = praw.Reddit(
-    client_id="CLIENT_ID",
-    client_secret="CLIENT_SECRET",
-    user_agent="USER_AGENT"
+    client_id=os.getenv('CLIENT_ID'),
+    client_secret=os.getenv('CLIENT_SECRET'),
+    user_agent=os.getenv('USER_AGENT')
 )
 
 for subreddit in subreddits:
