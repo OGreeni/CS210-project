65ab0e4d3de596f6c3605c991b460caa7ced0135
Leon Bash <leonbash@Leons-Air.torahlinks.org>
2024-07-12 14:37:54 -0400

    sentimental analysis file

--- main.py
+++ main.py
@@ -26,12 +26,6 @@ client = pymongo.MongoClient(os.getenv('MONGODB_URI'))
 db = client.reddit_data
 collection = db.posts
 
-# Initialize analyzer
-obj = SentimentIntensityAnalyzer()
-
-# Set up default dictionary
-sentiment_dict = defaultdict(list)
-
 found_tickers = []
 
 for subreddit in subreddits:
@@ -40,16 +34,6 @@ for subreddit in subreddits:
         cleaned = cleaning.remove_usernames(cleaning.remove_urls(submission.selftext))
         submission_tickers = tickers.find_tickers(cleaned) #Stores tickers found in each cleaned submission
         found_tickers.extend(tickers.find_tickers(cleaned))
-
-        # Calculate sentiment scores (including compound, pos, neu, neg) of the cleaned data
-        sentiment_scores = obj.polarity_scores(cleaned)
-
-        # Use compound score to determine whether the content is positive, negative, or neutral
-        sentiment = 'positive' if sentiment_scores['compound'] >= 0.1 else 'negative' if sentiment_scores['compound'] <= -0.1 else 'neutral'
-       
-        # Append the sentiment scores for each ticker found in the submission
-        for ticker in submission_tickers:
-            sentiment_dict[ticker].append(sentiment_scores)
 
         # Store the data of the posts
         post_data = {
@@ -61,28 +45,5 @@ for subreddit in subreddits:
         }
         collection.insert_one(post_data)
 
-average_sentiment_scores = {}
-
-# Averages sentiment scores across all submissions for each ticker
-for ticker, sentiments in sentiment_dict.items():
-    average_sentiment = {
-        'compound': np.mean([s['compound'] for s in sentiments]),
-        'pos': np.mean([s['pos'] for s in sentiments]),
-        'neu': np.mean([s['neu'] for s in sentiments]),
-        'neg': np.mean([s['neg'] for s in sentiments]),
-    }
-    average_sentiment_scores[ticker] = average_sentiment
-
-
-average_sentiment_collection = db.average_sentiments
-
-# Store the averages in a MongoDB collection
-for ticker, average_sentiment in average_sentiment_scores.items():
-    average_sentiment_collection.insert_one({
-        'ticker': ticker,
-        'average_sentiment': average_sentiment
-    })
-
 print("TICKERS FOUND: ", found_tickers)
-print("AVERAGE SENTIMENTS OF EACH TICKER: ", average_sentiment_scores)
 
